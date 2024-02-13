fcbd9ff2ac4a0769ec44e2aefb84f5640b295806
Dukewendo <dommct@yahoo.co.uk>
2024-02-13 20:00:31 +0000

    hard coded blog return

--- client/src/pages/users/Blogs.jsx
+++ client/src/pages/users/Blogs.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Page } from "../../components/page";
 
 export function Blogs() {
+	const { id } = useParams();
+
+	React.useEffect(() => {
+		fetch(`http://localhost:8000/post/1`, {
+			headers: { "Content-Type": "application/json" },
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Fetched user data:", data);
+				setUser(data);
+			})
+			.catch((err) => {
+				console.error("Error fetching user data:", err);
+				setError(err.message || "An error occurred.");
+			});
+	}, [id]);
+
 	return (
 		<Page>
 			<div>Here are the bloggs</div>
-			{/* fetch request to display all blogs and possible clickable link to open one */}
 		</Page>
 	);
 }
